c5ec34dd0be8ccddc2a8ab605e7beb851ec4fd12
designwithpash <designwithpash@gmail.com>
2024-11-12 23:08:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const swiper = new Swiper('.swiper', {
-    effect: "coverflow",
+var swiper = new Swiper('.swiper', {
+
     spaceBetween: 30,
     grabCursor: true,
     centeredSlides: true,
@@ -44,3 +44,55 @@ const swiper = new Swiper('.swiper', {
 
     },
 });
+
+var swipertwo = new Swiper('.swiper2',{
+
+  spaceBetween: 30,
+  grabCursor: true,
+  centeredSlides: true,
+  keyboard: { enable: true },
+  loop: true,
+  // If we need pagination
+  pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+      clickable: true,
+  },
+
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+
+
+  breakpoints: {
+
+     640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+
+      },
+      728: {
+        slidesPerView: 1,
+        spaceBetween: 40,
+      
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+
+  },
+
+
+
+})
+
+$(".secondary-button").on('click', (evt) => {
+  $(".secondary-button").removeClass('active');
+  $(evt.currentTarget).addClass('active');
+  $(evt.currentTarget).css({
+    "transition":"0.5 ease-in"
+  })
+
+})
